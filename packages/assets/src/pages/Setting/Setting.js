b0e30df26cc2a3cd5811302fde6114ecfa962b30
nguyenhoangtien <hoangtienflytom@gmail.com>
2023-09-21 11:16:24 +0700

    refactor code

--- packages/assets/src/pages/Setting/Setting.js
+++ packages/assets/src/pages/Setting/Setting.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useState} from 'react';
 import {Card, Layout, Page, Tabs, Loading} from '@shopify/polaris';
 import NotificationPopup from '../../components/NotificationPopup/NotificationPopup';
 import '../../styles/pages/setting.scss';
